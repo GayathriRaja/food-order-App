5f4392ef2abe3cefe65729b2c652cb0525fbb5f9
Gayathri <gayathrraja@gmail.com>
2021-02-17 17:01:48 +0530

    Updates

---
+++ precache-manifest.2a59505f98e814cf7bfbd31ed63d884d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189aa6edf43466fa25d0fe4ffd755f3f",
+    "url": "/Food-Order-App/index.html"
+  },
+  {
+    "revision": "3352102bc8ff4f33af2c",
+    "url": "/Food-Order-App/static/css/main.8d3b6155.chunk.css"
+  },
+  {
+    "revision": "bf9219a8e53367ca8723",
+    "url": "/Food-Order-App/static/js/2.b6ede45d.chunk.js"
+  },
+  {
+    "revision": "d2f5423b62d53b191f17197aeaf59a01",
+    "url": "/Food-Order-App/static/js/2.b6ede45d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3352102bc8ff4f33af2c",
+    "url": "/Food-Order-App/static/js/main.5ac5caeb.chunk.js"
+  },
+  {
+    "revision": "7e44058827bc574af763",
+    "url": "/Food-Order-App/static/js/runtime-main.83872bad.js"
+  },
+  {
+    "revision": "e1de8a11d8d103c3d816435915b20cd2",
+    "url": "/Food-Order-App/static/media/GImage1.e1de8a11.jpg"
+  },
+  {
+    "revision": "f26da01220e6e2c3fe906343a08ed497",
+    "url": "/Food-Order-App/static/media/leftSideImage3.f26da012.jpg"
+  }
+]);
